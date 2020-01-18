e01a3d61eea0e3c239b2efbdfb3ce77220a07a6b
Ozan Dogrultan <ozan@hipo.biz>
2020-01-18 12:28:13 +0300

    Fixed removing of existing labels issue

--- app.py
+++ app.py
@@ -14,21 +14,26 @@ app = Flask(__name__)
 @app.route("/github-webhook/", methods=["POST"])
 def add_has_migration_label():
     """
-    This resource acts as a webhook to add "has_migration" label to a PR
+    This endpoint acts as a webhook to add "has_migration" label to a PR
     when a Django migration is introduced.
     """
     s = requests.Session()
-    payload = json.loads(request.get_data())
-    url = payload["pull_request"]["url"]
-    pr_url = url.split(BASE_API_ENDPOINT)[-1]
     if GITHUB_AUTH_TOKEN:
         s.headers.update({"Authorization": f"token {GITHUB_AUTH_TOKEN}"})
+    payload = json.loads(request.get_data())
+    url = payload["pull_request"]["url"]
+    pr_endpoint = url.split(BASE_API_ENDPOINT)[-1]
+    pr_url = BASE_API_ENDPOINT + pr_endpoint
 
-    r = s.get(BASE_API_ENDPOINT + pr_url + "/files")
-    for file_dict in r.json():
+    pr_files_response = s.get(pr_url + "/files")
+    for file_dict in pr_files_response.json():
         if "migrations/" in file_dict["filename"]:
+            pr_response = s.get(pr_url)
+            existing_labels = [label["name"] for label in pr_response.json()["labels"]]
+            # Github API uses "issues" resource for PRs as well
             s.patch(
-              BASE_API_ENDPOINT + pr_url.replace("pulls/", "issues/"),
-              data=json.dumps({"labels": ["has migration"]})
+              BASE_API_ENDPOINT + pr_endpoint.replace("pulls/", "issues/"),
+              data=json.dumps({"labels": list(set(existing_labels + ["has migration"]))})
             )
+            break
     return "OK"
